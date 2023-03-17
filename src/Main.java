8ab52116db502d9a09924db41632bc8c4b16f382
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 13:53:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en textfil och skriv ut innehållet på skärmen,
+        // men översätt alla aa, ae, oe till å, ä, och ö
+
+
+    }
+}
